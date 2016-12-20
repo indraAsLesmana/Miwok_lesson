b35acc603723c8854b6e3a3ed4be6378b971fa08
Indra Agus Lesmana <indra.agus@kiranatama.com>
2016-12-20 11:46:29 +0700

    add title side icon, change text color if tab selected

--- app/src/main/java/com/example/android/miwok/adapter/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/adapter/SimpleFragmentPagerAdapter.java
@@ -66,7 +66,7 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter{
         // return tabTitles[position];
         Drawable image = ContextCompat.getDrawable(mContext, imageResId_white[position]);
         image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
-        SpannableString sb = new SpannableString(" ");
+        SpannableString sb = new SpannableString("   "+ tabTitles[position]);
         ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
         sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         return sb;
